213ca2b4f571569691e8133232ad18c761398153
Nav-03 <navil.gabrie@gmail.com>
2022-03-30 06:20:15 -0400

    initial commit

---
+++ src/PathfindingVisualizer/Node/PathfindingVisualizer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Node from './Node';
+
+import '../src/PathfindingVisualizer/Node/PathfindingVisualizer.jsx';
+
+export const PathfindingVisualizer = (props) => {
+
+
+  return (
+    <div>
+      Foo
+      <Node></Node>
+    </div>
+  );
+}
