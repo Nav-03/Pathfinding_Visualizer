4659af10a737fc30397035498076a365eb1adb5c
Nav-03 <navil.gabrie@gmail.com>
2022-03-31 18:02:39 -0400

    updated to main page

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Node } from './Node/Node';
 
 import './PathfindingVisualizer.css';
 
 
+
+
+componentDidMount() {
+  const nodes = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow = [];
+    for (let col = 0, col < 50, col++) {
+      currentRow.push([]);
+    }
+    nodes.push(currentRow);
+  }
+  this.setState({ nodes });
+}
+
 export const PathfindingVisualizer = () => {
 
   return (
-    <div>
-      Foo
-      <Node></Node>
+    <div className="grid">
+      {nodes.map((row, rowIdx) => {
+        return <div>
+          {row.map((node, nodeIdx) => <Node></Node>)}
+        </div>;
+      })}
     </div>
   )
 }
